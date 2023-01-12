83086d2028a4c11d1c8200aac2f5b21ad4dc18a9
gothmania <longhoang1712@outlook.com>
2023-01-12 22:55:59 +0700

    Add py_you 01.

---
+++ pytutor_youtube/01_liststrfor.py
@@ -0,0 +1,44 @@
+"""
+PYTUTOR - YOUTUBE
+Tập 01: list, str, for
+
+  Trong tập này, các bạn sẽ làm quen với kiểu dữ liệu chuỗi kí tự (str),
+danh sách (list), và sử dụng vòng lặp for. Chúng ta cũng sẽ làm quen với
+một kĩ thuật lặp quan trọng tên là list comprehension.
+
+
+NHIỆM VỤ
+--------
+Từ tên của một người tạo ra tên viết tắt, ví dụ: từ "Hoàng Bảo Long" tạo
+ra "HBL".
+
+"""
+
+
+# Lưu tên người trong chuỗi kí tự
+name = "Hoàng Bảo  Long"
+
+
+### BƯỚC 1: Tách (split) tên thành các phần của tên
+# như họ, đệm, tên riêng
+
+
+# Nếu thiết lập đối số `sep=" "` thì kết quả có gì khác?
+
+
+
+### BƯỚC 2: Danh sách (list)
+# - Danh sách có mấy phần tử?
+# - Truy cập vào các phần tử trong list
+# - Lặp qua các phần tử
+
+
+### BƯỚC 3: Chuỗi kí tự (str)
+# - Truy cập vào các kí tự trong chuỗi kí tự
+# - Ghép các chuỗi kí tự
+
+
+### BƯỚC 4: List comprehension
+# - Bổ sung phần tử vào danh sách
+# - Kết nối các chuỗi kí tự trong list thành một chuỗi kí tự
+# - Thu gọn thao tác bằng list comprehension
